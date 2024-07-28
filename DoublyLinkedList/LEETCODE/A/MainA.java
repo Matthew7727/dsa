54795047078bc68c58cd4c7c7abfea81b38c1a3b
Eccleston <matthew.eccleston17@gmail.com>
2024-07-28 16:33:10 +0100

    Q1

---
+++ DoublyLinkedList/LEETCODE/A/MainA.java
@@ -0,0 +1,44 @@
+package DoublyLinkedList.LEETCODE.A;
+
+public class MainA {
+
+
+    public static void mainA(String[] args) {
+
+        DoublyLinkedListA myDLL = new DoublyLinkedListA(1);
+        myDLL.append(2);
+        myDLL.append(3);
+        myDLL.append(4);
+        myDLL.append(5);
+        
+        System.out.println("DLL before swap:");
+        myDLL.printList();
+        
+        myDLL.swapFirstLast();
+        
+        System.out.println("\nDLL after swap:");
+        myDLL.printList();
+        
+        
+        /*
+       		EXPECTED OUTPUT:
+        	----------------
+            DLL before swap:
+            1
+            2
+            3
+            4
+            5
+            
+            DLL after swap:
+            5
+            2
+            3
+            4
+            1
+        
+     	*/
+
+    }
+    
+}
