ac5a4837c7d729240491976e9ae54e97af836642
Eccleston <matthew.eccleston17@gmail.com>
2024-07-28 15:50:16 +0100

    Doubly Linked List :)

--- Main.java
+++ Main.java
@@ -5,14 +5,20 @@ import LinkedList.LinkedList;
 public class Main {
   public static void main(String[] args) {
     System.out.println("hello world");
-    linkedlist();
+    // linkedlist();
     doublyLinkedList();
   }
 
   public static void doublyLinkedList() {
     DoublyLinkedList myDLinkedList = new DoublyLinkedList(7);
     myDLinkedList.append(5);
+    myDLinkedList.append(8);
+    myDLinkedList.append(78);
+    myDLinkedList.insert(2, 3);
+    myDLinkedList.insert(2, 4);
+    myDLinkedList.remove(1);
     myDLinkedList.printList();
+
   }
 
   public static void linkedlist() {
